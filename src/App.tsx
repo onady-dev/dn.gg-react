8196908b3ba0bb99638a52173379846c8469869c
onady <onady.dev@gmail.com>
2024-04-16 23:39:16 +0900

    마커 표시, 클릭 이벤트 함수 생성

--- src/App.tsx
+++ src/App.tsx
@@ -6,34 +6,92 @@ declare global {
   }
 }
 
+interface Court {
+  id: number;
+  name: string;
+  address: string;
+  homepage: string;
+}
+const courts: Court[] = [
+  {
+    id: 0,
+    name: "가천대학교(글로벌)",
+    address: "경기 성남시 수정구 성남대로 1342",
+    homepage: "https://shareit.kr/venue/548",
+  },
+  {
+    id: 1,
+    name: "어시스트",
+    address: "인천 남동구 고잔로102번길 50 어시스트 체육관",
+    homepage: "https://shareit.kr/venue/6060",
+  },
+  {
+    id: 2,
+    name: "구리토평체육관",
+    address: "경기 구리시 벌말로128번길 48",
+    homepage: "https://shareit.kr/venue/3804",
+  },
+];
+
 const App: React.FC = () => {
+  const addressToLatLng = async (address: string) => {
+    const geocoder = new window.kakao.maps.services.Geocoder();
+    const result = new Promise<void>((resolve, reject) => {
+      geocoder.addressSearch(address, function (result: any, status: any) {
+        if (status === window.kakao.maps.services.Status.OK) {
+          resolve(result[0]);
+        } else {
+          console.log("잘못된 주소 입력");
+        }
+      });
+    });
+    return result;
+  };
+
+  const setInfoWindow = (content: string, position: string) => {
+    const iwContent = `<div style="padding:5px;">${content}</div>`;
+    const infoWindow = new window.kakao.maps.InfoWindow({
+      position,
+      content: iwContent,
+    });
+    return infoWindow;
+  };
+
+  const setMarkerMouseEvent = (map: any, marker: any, infoWindow: any, court: Court) => {
+    window.kakao.maps.event.addListener(marker, "click", function () {
+      window.open(court.homepage);
+    });
+    window.kakao.maps.event.addListener(marker, "mouseover", function () {
+      infoWindow.open(map, marker);
+    });
+    window.kakao.maps.event.addListener(marker, "mouseout", function () {
+      infoWindow.close();
+    });
+  };
+
   useEffect(() => {
     window.kakao.maps.load(() => {
+      const defaultLatLng = new window.kakao.maps.LatLng(37.566826, 126.9786567);
       const container = document.getElementById("map");
       const options = {
-        center: new window.kakao.maps.LatLng(37.566826, 126.9786567), // 서울 시청 좌표
-        level: 3,
+        center: defaultLatLng,
+        level: 10,
       };
       const map = new window.kakao.maps.Map(container, options);
-      const clusterer = new window.kakao.maps.MarkerClusterer({
-        map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체
-        averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
-        minLevel: 10, // 클러스터 할 최소 지도 레벨
+
+      courts.map((court: Court) => {
+        addressToLatLng(court.address).then((position: any) => {
+          const latLng = new window.kakao.maps.LatLng(position.y, position.x);
+          const marker = new window.kakao.maps.Marker({
+            map,
+            position: latLng,
+            clickable: true,
+          });
+
+          const infoWindow = setInfoWindow(court.name, latLng);
+          setMarkerMouseEvent(map, marker, infoWindow, court);
+        });
       });
-      clusterer.addMarkers([
-        new window.kakao.maps.Marker({
-          position: new window.kakao.maps.LatLng(37.566826, 126.9786567),
-        }),
-        new window.kakao.maps.Marker({
-          position: new window.kakao.maps.LatLng(37.666826, 126.8786567),
-        }),
-        new window.kakao.maps.Marker({
-          position: new window.kakao.maps.LatLng(37.566826, 126.8786567),
-        }),
-        new window.kakao.maps.Marker({
-          position: new window.kakao.maps.LatLng(37.666826, 126.9786567),
-        }),
-      ]);
     });
   }, []);
 
